4fd4719edcaf36a4e0773c4a047bfe351acae7c5
Artem Zakirullin <artemzr@gmail.com>
2024-07-07 21:43:37 +0300

    add test for last month

--- internal/insights/insights_test.go
+++ internal/insights/insights_test.go
@@ -2,6 +2,7 @@ package insights
 
 import (
 	_ "embed"
+	"fmt"
 	"testing"
 
 	"github.com/spf13/afero"
@@ -12,6 +13,9 @@ import (
 
 //go:embed testdata/month_habits.md
 var monthMD string
+
+//go:embed testdata/last_month_habits.md
+var lastMonthMD string
 
 func TestRead(t *testing.T) {
 	r := require.New(t)
@@ -34,6 +38,32 @@ func TestRead(t *testing.T) {
 	r.Equal(false, completed)
 
 	completed, ok = year[31]
+	r.True(ok)
+	r.Equal(true, completed)
+}
+
+func TestReadLastMonthHabits(t *testing.T) {
+	r := require.New(t)
+
+	botFS, err := fs.NewFS("/", afero.NewMemMapFs())
+	r.NoError(err)
+	botFS.Put(fs.DirInsights,  "1970 Habits.md",  lastMonthMD)
+
+	habits, err := Read(botFS, 1970)
+	r.NoError(err)
+
+	r.Len(habits, 1)
+	year, ok := habits["Habit"]
+	r.True(ok)
+
+	r.Len(year, 31)
+
+	fmt.Printf("%v", year)
+	completed, ok := year[335]
+	r.True(ok)
+	r.Equal(false, completed)
+
+	completed, ok = year[365]
 	r.True(ok)
 	r.Equal(true, completed)
 }
