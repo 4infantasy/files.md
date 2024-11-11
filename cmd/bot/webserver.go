55597655e813db4e202e74289f5ce2751174f760
Artem Zakirullin <artemzr@gmail.com>
2024-11-11 18:06:58 +0200

    add editor html as home page

--- cmd/bot/webserver.go
+++ cmd/bot/webserver.go
@@ -98,7 +98,13 @@ func habitsServer(habitsHost, certDir, logFilename string) {
 
 func setupRouter(router *http.ServeMux, logger *log.Logger) {
 	fileServer := http.FileServer(http.Dir("./editor"))
-	router.Handle("/app/", http.StripPrefix("/app", fileServer))
+	router.HandleFunc("/app", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/app" || r.URL.Path == "/app/" {
+			http.ServeFile(w, r, "./editor/editor.html")
+			return
+		}
+		http.StripPrefix("/app", http.FileServer(http.Dir("./editor"))).ServeHTTP(w, r)
+	})
 
 	// TODO add hashing or secrets
 	// TODO before release habits_v2 => habits
